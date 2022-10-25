884b7c586d56b6bc3275525ebcc55259992f4db3
rogowska <rogowska.oliwia@gmail.com>
2022-10-25 09:24:11 +0200

    Usuniecie komentarzy

--- src/pl/edu/uj/javaframe/Main.java
+++ src/pl/edu/uj/javaframe/Main.java
@@ -64,20 +64,6 @@ public class Main {
         System.out.println("------------------------------"); */
 
 
-//      INSTRUKCJE DO ĆWICZEŃ I
-
-//      Dopisz metodę print w klasie DataFrame wypisującą zawartość dataframe
-//      df.print();
-
-//      Dodaj klasę ImaginaryDouble oraz ImaginaryInt, które dziedziczą po odpowiednio klasach Int i Double . Powinne one być tworzone ze Stringów postaci "<CzęśćRzeczywissta>i<CzęśćUrojona>", np "12i3".
-
-//      Nadpisz metody create i add w klasach ImaginaryDoubgle oraz ImaginaryInt. Pamiętaj, że do liczb urojonych możemy też dodawać inne typy (np. Int, Double).
-//      Przetestuj wyniki operacji:
-        //        ImaginaryInt.create("12i4").add(Int.create("10"))
-        //        Int.create("10").add(ImaginaryInt.create("12i4")))
-        //        ImaginaryInt.create("12i4").add(ImaginaryInt.create("10i10"))
-
-//      Nadpisz metodę public String toString() w kasach ImaginaryDouble i ImaginaryInt, tak aby poprawnie wyświetlała liczby urojone do postaci Stringa
 
 
 
