3d0bb7da2cae3eca4092dda537b8cf88ad666029
rogowska <rogowska.oliwia@gmail.com>
2023-01-14 16:16:48 +0100

    echo server division update

--- java2020/EchoServer/EchoServer.java
+++ java2020/EchoServer/EchoServer.java
@@ -26,17 +26,20 @@ public class EchoServer {
 
         while ((inputLine = in.readLine()) != null) {
             String[] exp = inputLine.split("\\s");
-            if(exp.length != 3){
-                out.println("Wrong format of expression, please use: \"number operator number\"");
+            if (exp.length != 3) {
+                out.println("Wrong format of expression, please use: \"number operator number\", exiting.");
             }
             Double[] numbers = new Double[2];
             String operator = exp[1];
             try {
                 numbers[0] = Double.valueOf(exp[0]);
                 numbers[1] = Double.valueOf(exp[2]);
-            }
-            catch(NumberFormatException e){
+            } catch (NumberFormatException e) {
                 out.println("NumberFormatException, exiting.");
+                out.close();
+                in.close();
+                clientSocket.close();
+                serverSocket.close();
             }
             if (operator.equals("+")) {
                 out.println(numbers[0] + numbers[1]);
@@ -45,14 +48,14 @@ public class EchoServer {
             } else if (operator.equals("*")) {
                 out.println(numbers[0] * numbers[1]);
             } else if (operator.equals("/")) {
-                if(numbers[1] == 0){
+                if (numbers[1] == 0) {
                     out.println("Division by 0, please enter another expression");
                 }
-                out.println(numbers[0] / numbers[1]);
+                Double result = numbers[0] / numbers[1];
+                out.println(result);
             } else {
                 out.println("Wrong operator");
             }
-
         }
         out.close();
         in.close();
